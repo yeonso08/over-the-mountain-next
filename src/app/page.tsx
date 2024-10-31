7718b90c48f26e1ccc8426949df7a7f682784db4
jaeang <hjng0825@gmail.com>
2024-10-31 15:18:01 +0900

    feat : eslint, prettierrc 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import {Button} from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-   <div>
-     <Button>test</Button>
+    <div>
+      <Button>test</Button>
     </div>
-  );
+  )
 }
